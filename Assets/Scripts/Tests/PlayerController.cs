f3911446e53af71da4e6382ba998b0184c385423
AirDryingFish <61225980+AirDryingFish@users.noreply.github.com>
2026-01-24 20:37:52 +0800

    Add space jump

--- Assets/Scripts/Tests/PlayerController.cs
+++ Assets/Scripts/Tests/PlayerController.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10.0f;
+    public float jumpForce = 5.0f;           // 跳跃力度
     public SpriteRenderer spriteRenderer;
     private bool moveRight;
+    private Rigidbody2D rb;
+    private bool isGrounded = true;          // 简单的落地标记
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,9 @@ public class PlayerController : MonoBehaviour
         {
             spriteRenderer = GetComponent<SpriteRenderer>();
         }
+
+        rb = GetComponent<Rigidbody2D>();
+
         moveRight = false;
         if (Input.GetAxis("Horizontal") > 0)
         {
@@ -28,6 +35,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 水平移动（保持原来的写法）
         if (Input.GetAxisRaw("Horizontal") > 0)
         {
             this.transform.position += new Vector3(1, 0, 0) * speed * Time.deltaTime;
@@ -39,19 +47,30 @@ public class PlayerController : MonoBehaviour
             moveRight = false;
         }
 
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        // 空格跳跃，只在着地时才能起跳
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            this.transform.position += new Vector3(0, 1, 0) * speed * Time.deltaTime;
+            Debug.Log("Jump pressed");
+            isGrounded = false;
+            rb.velocity = new Vector2(rb.velocity.x, 0f); // 清空当前竖直速度，保证跳跃一致
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-        
-        if (moveRight) 
+
+        // 根据朝向翻转
+        if (moveRight)
         {
-            spriteRenderer.flipX = false;   
+            spriteRenderer.flipX = false;
         }
         else
         {
             spriteRenderer.flipX = true;
         }
+    }
+
+    // 简单的落地检测：与任何碰撞体接触都算落地
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 如果想更精细，可以判断 collision.gameObject.tag == "Ground"
+        isGrounded = true;
     }
 }

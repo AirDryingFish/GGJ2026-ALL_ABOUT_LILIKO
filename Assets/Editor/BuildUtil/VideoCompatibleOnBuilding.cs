3e7a4fc15c92a04244081eb593feb1bc022a3604
AntltNoProb <gingerant@sina.com>
2026-02-04 05:17:03 +0800

    fix building error

---
+++ Assets/Editor/BuildUtil/VideoCompatibleOnBuilding.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+public class VideoCompatibleOnBuilding : IPreprocessBuildWithReport, IPostprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    static readonly string TEMP_ROOT = "Temp/WebGLCompatibleTEMP";
+
+
+    public void OnPreprocessBuild(BuildReport report)
+    {
+        bool isWebGL = report.summary.platform == BuildTarget.WebGL;
+
+        if (!isWebGL)
+        {
+            return;
+        }
+
+
+        var compas = Resources.FindObjectsOfTypeAll<VideoPlayerWebCompatible>();
+        foreach (var compa in compas)
+        {
+            if (!compa.useClip || compa.clip == null)
+            {
+                continue;
+            }
+            string vdPath = AssetDatabase.GetAssetPath(compa.clip);
+            string dst = CopyVideoToTemp(vdPath, compa.webglCompatiblePath);
+            Debug.Log($"Copy from {vdPath} to temp {dst}");
+        }
+    }
+
+    public string CopyVideoToTemp(string path, string to)
+    {
+        string tmpRoot = Path.Combine(TEMP_ROOT, VideoPlayerWebCompatible.VIDEO_PATH);
+        string dst = Path.Combine(tmpRoot, to.TrimStart('/','\\'));
+        Directory.CreateDirectory(Path.GetDirectoryName(dst));
+        // FileUtil.CopyFileOrDirectory(path, dst);
+        FileUtil.ReplaceFile(path, dst);
+        return dst;
+    }
+
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        string tmpRoot = Path.Combine(TEMP_ROOT, VideoPlayerWebCompatible.VIDEO_PATH);
+        string saRoot = Path.Combine(report.summary.outputPath, VideoPlayerWebCompatible.VIDEO_PATH);
+        FileUtil.CopyFileOrDirectory(tmpRoot, saRoot);
+    }
+}

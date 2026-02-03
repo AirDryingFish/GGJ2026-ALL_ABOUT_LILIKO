64b0787c0a8397523687339c3bb798ab7211d55a
AntltNoProb <gingerant@sina.com>
2026-02-04 06:15:02 +0800

    fix: now search from all scenes

--- Assets/Editor/BuildUtil/VideoCompatibleOnBuilding.cs
+++ Assets/Editor/BuildUtil/VideoCompatibleOnBuilding.cs
@@ -4,6 +4,7 @@ using System.IO;
 using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class VideoCompatibleOnBuilding : IPreprocessBuildWithReport, IPostprocessBuildWithReport
@@ -23,7 +24,8 @@ public class VideoCompatibleOnBuilding : IPreprocessBuildWithReport, IPostproces
         }
 
 
-        var compas = Resources.FindObjectsOfTypeAll<VideoPlayerWebCompatible>();
+        // var compas = Resources.FindObjectsOfTypeAll<VideoPlayerWebCompatible>();
+        var compas = SearchAllScene<VideoPlayerWebCompatible>();
         foreach (var compa in compas)
         {
             if (!compa.useClip || compa.clip == null)
@@ -34,6 +36,29 @@ public class VideoCompatibleOnBuilding : IPreprocessBuildWithReport, IPostproces
             string dst = CopyVideoToTemp(vdPath, compa.webglCompatiblePath);
             Debug.Log($"Copy from {vdPath} to temp {dst}");
         }
+    }
+
+    public List<T> SearchAllScene<T>()
+    {
+         if (!EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            throw new BuildFailedException("Cancelled due to unsaved scenes.");
+
+        var all = new List<T>();
+
+        foreach (var s in EditorBuildSettings.scenes)
+        {
+            if (!s.enabled) continue;
+
+            var scene = EditorSceneManager.OpenScene(s.path, OpenSceneMode.Additive);
+
+            foreach (var root in scene.GetRootGameObjects())
+                all.AddRange(root.GetComponentsInChildren<T>(true)); // true: include inactive
+
+            EditorSceneManager.CloseScene(scene, removeScene: true);
+        }
+
+        Debug.Log($"[Prebuild] Found {all.Count} {nameof(T)} in all Scenes.");
+        return all;
     }
 
     public string CopyVideoToTemp(string path, string to)
@@ -50,6 +75,8 @@ public class VideoCompatibleOnBuilding : IPreprocessBuildWithReport, IPostproces
     {
         string tmpRoot = Path.Combine(TEMP_ROOT, VideoPlayerWebCompatible.VIDEO_PATH);
         string saRoot = Path.Combine(report.summary.outputPath, "StreamingAssets", VideoPlayerWebCompatible.VIDEO_PATH);
-        FileUtil.CopyFileOrDirectory(tmpRoot, saRoot);
+        // FileUtil.CopyFileOrDirectory(tmpRoot, saRoot);
+        Debug.Log($"Copy from {tmpRoot} to temp {saRoot}");
+        FileUtil.ReplaceDirectory(tmpRoot, saRoot);
     }
 }
